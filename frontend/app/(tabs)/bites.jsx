12c7b47f6e5e2443e445ba70938e57adc80e3d31
myathetchai <myathetchai441@gmail.com>
2024-09-15 09:13:25 +0800

    sort and update data

--- frontend/app/(tabs)/bites.jsx
+++ frontend/app/(tabs)/bites.jsx
@@ -5,6 +5,9 @@ import { data } from '../../data';
 
 export default function Bites() {
 
+  const sortedData = data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+
   const renderBite = ({ item }) => (
     <View style={styles.itemContainer}>
       <Image source={{ uri: item.image }} style={styles.image} />
@@ -19,7 +22,7 @@ export default function Bites() {
         <Text style={styles.welcomeText}>My previous bites</Text>
       </View>
       <FlatList
-        data={data}
+        data={sortedData}
         renderItem={renderBite}
         keyExtractor={(item, index) => index.toString()}
         contentContainerStyle={styles.listContainer}
